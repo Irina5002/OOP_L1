3ed561ab4366a3fe76b5083f86e7df9079981753
Irina5002 <irina.komarova5002@mail.ru>
2022-04-26 16:15:54 +0300

    Добавьте файлы проекта.

---
+++ Lab_1.1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Lab_1._1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        NumberGenerator Number;
+
+        public WeatherForecastController (NumberGenerator a)
+        {
+            Number = a;
+        }
+
+        [HttpGet]
+        public int Generate()
+        {
+            return (Number.Generate());
+        }
+    }
+}
---
+++ Lab_1.1/NumberGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_1._1
+{
+    public class NumberGenerator
+    {
+        public int Generate()
+        {
+            Random rnd = new Random();
+            return rnd.Next(1, 100);
+        }
+    }
+}
